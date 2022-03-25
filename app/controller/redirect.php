fbf190505bd9dedc0fabe15da8eb1f02632518d1
Gustavo Baierski <gustavobaierski@gmail.com>
2022-03-25 01:42:39 -0300

    Criar redirecionamento de páginas

---
+++ app/controller/redirect.php
@@ -0,0 +1,16 @@
+<?php
+
+function telaLogin() {
+    $nomePagina = "Tela de Login | Box";
+    require '../view/telaLogin.php';
+}
+
+//Gerenciador de Rotas
+if (isset($_GET['action']) and function_exists($_GET['action']) ) {
+    call_user_func($_GET['action']);
+    $_SESSION['local'] = "Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+} else {
+    $_SESSION['local'] = "Location: http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,1 @@
+<?php header("location: app/controller/redirect.php?action=telaLogin"); ?>
